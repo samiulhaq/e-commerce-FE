c2ec58f32f4df13c97da048cefcd10553987c7cd
webdev2 <webdev2@NOCMAC08.local>
2018-03-22 16:14:26 +0400

    Adding Material UI

--- bestinpk-fe/src/app/app.module.ts
+++ bestinpk-fe/src/app/app.module.ts
@@ -1,5 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule, MatMenuModule} from '@angular/material';
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule, 
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
